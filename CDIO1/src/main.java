24533a7c04dd21b6a14e543546ae8fdbcaeea6f9
Michael Westergaard <michaelwestergaard@hotmail.dk>
2018-02-17 15:53:32 +0100

    main metode

---
+++ CDIO1/src/main.java
@@ -0,0 +1,16 @@
+import simpel2lags.DemoData;
+import simpel2lags.IData;
+import simpel2lags.IUI;
+import simpel2lags.TUI;
+
+public class main {
+
+	public static void main(String[] args) {
+//		IData data = new DemoData();
+//		IUI ui = new TUI(data);
+		while (true){
+		   ui.showMenu();
+		}
+	}
+
+}
