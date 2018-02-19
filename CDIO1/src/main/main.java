0a9fb0b49025596859ee18d94aca8f5bf804ab89
Michael <Michael@Michael-Laptop.clients.wireless.dtu.dk>
2018-02-19 15:52:44 +0100

    Fjernelse af comments

--- CDIO1/src/main/main.java
+++ CDIO1/src/main/main.java
@@ -23,52 +23,7 @@ public class main {
 		IUI ui = new TUI(data);
 		while (true){
 		   ui.showMenu();
-		}
-		
-//		UserDTO dto = new UserDTO(2, "rtest", "test", "test", "test", "test");
-//		FileOutputStream f;
-//		try {
-//			f = new FileOutputStream(new File("myObjects.txt"));
-//			ObjectOutputStream o = new ObjectOutputStream(f);
-//
-//			// Write objects to file
-//			o.writeObject(dto);
-//
-//			o.close();
-//			f.close();
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		FileInputStream fi;
-//		try {
-//			FileInputStream fi = new FileInputStream(new File("myObjects.txt"));
-//			ObjectInputStream oi = new ObjectInputStream(fi);
-//
-//			// Read objects
-//			UserDTO pr1 = (UserDTO) oi.readObject();
-//
-//			System.out.println(pr1.toString());
-//
-//			oi.close();
-//			fi.close();
-//			
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		
+		}	
 		
 	}
 
